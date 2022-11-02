87bd176600a4c1db4ab24c5e5c57ef45c9c45283
hwelsters <corevalue.noel@gmail.com>
2022-11-02 01:40:20 -0700

    added copyright footer

--- client/src/layouts/Footer/index.tsx
+++ client/src/layouts/Footer/index.tsx
@@ -4,24 +4,27 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <div className={styles.root}>
-      <div className={styles.logo_column}>
-        <Logo />
-      </div>
+      <div className={styles.top}>
+        <div className={styles.logo_column}>
+          <Logo />
+        </div>
 
-      
-      <div className={styles.column}>
-        <h3>Yoga</h3>
+        <div className={styles.column}>
+          <h3>Yoga</h3>
+        </div>
+        <div className={styles.column}>
+          <h3>Wellbeing</h3>
+        </div>
+        <div className={styles.column}>
+          <h3>Resources</h3>
+          <span className={styles.list_item}>About Us</span>
+          <span className={styles.list_item}>Copyright Policy</span>
+          <span className={styles.list_item}>Terms of Service</span>
+          <span className={styles.list_item}>Privacy Policy</span>
+        </div>
       </div>
-      <div className={styles.column}>
-        <h3>Wellbeing</h3>
-      </div>
-      <div className={styles.column}>
-        <h3>Resources</h3>
-        <span className={styles.list_item}>About Us</span>
-        <span className={styles.list_item}>Copyright Policy</span>
-        <span className={styles.list_item}>Terms of Service</span>
-        <span className={styles.list_item}>Privacy Policy</span>
-      </div>
+      <br/><br/><br/><br/><br/><br/><br/>
+      <div className={styles.footer_text}>© Copyright by hwelsters (but not really)</div>
     </div>
   );
 }
