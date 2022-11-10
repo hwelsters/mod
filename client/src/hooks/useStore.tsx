c3e7d88fc5a694f1f543db694fdd0f74e95914d7
hwelsters <corevalue.noel@gmail.com>
2022-11-09 21:34:19 -0700

    db

--- client/src/hooks/useStore.tsx
+++ client/src/hooks/useStore.tsx
@@ -1,7 +1,11 @@
-import { apiGet } from "services/api"
+import { apiGet, apiPost } from "services/api"
 
 export const useStore = () => {
     const getitems = () => {
         return apiGet("api/shop/getitems")
+    }
+
+    const buy = (id : string) => {
+        return apiPost("api/shop/buy", {itemId: id, userId: id});
     }
 }
--- client/src/layouts/Navbar/components/NavbarModal/NavbarModal.tsx
+++ client/src/layouts/Navbar/components/NavbarModal/NavbarModal.tsx
@@ -1,19 +1,30 @@
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LogoutIcon from "@mui/icons-material/Logout";
+import pageURLs from "data/pageURLs";
+import { useAuth } from "hooks/useAuth";
 import NavbarModalIcons from "../NavbarModalIcons/NavbarModalIcons";
 
 import styles from "./NavbarModal.module.css";
 
-
 export default function NavbarModal() {
+  const { logout } = useAuth();
+
+  const profile = (e: any) => {
+    window.location.href = pageURLs.profile;
+  };
+
+  const signout = (e: any) => {
+    logout();
+  };
+
   return (
     <div className={styles.navbarModal__root}>
-      <div>
+      <div onClick={profile}>
         <NavbarModalIcons Icon={AccountCircleIcon} text="Profile" />
       </div>
 
       {/* Logout user on click */}
-      <div>
+      <div onClick={signout}>
         <NavbarModalIcons Icon={LogoutIcon} text="Sign out" />
       </div>
     </div>
